8ec9a948f7e527d66438b037754c4c68e01b2b1d
Fredrik <fredrikf2000@gmail.com>
2021-07-05 19:29:18 +0200

    project start

---
+++ firebase.js
@@ -0,0 +1,8 @@
+const firebaseConfig = {
+  apiKey: "AIzaSyAka0Rm1wKbezgTbizrANht5UC2wu6Pwdg",
+  authDomain: "signal-clone-952e1.firebaseapp.com",
+  projectId: "signal-clone-952e1",
+  storageBucket: "signal-clone-952e1.appspot.com",
+  messagingSenderId: "861117788961",
+  appId: "1:861117788961:web:8e22dbc8cd35079a96c460",
+};
